52d218d87429fa8daef55e5ba54ff2fe327cf31c
bezzy0515 <beselaere@hotmail.com>
2024-10-07 20:07:38 -0400

    Step #1 Complete

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,1 +1,14 @@
+import pytest
+import prime
 
+"""
+Step #1
+Verify data type is integer.
+"""
+
+
+def test_data_not_integer():
+    with pytest.raises(ValueError):
+        prime.generate_prime_factors("test")
+    with pytest.raises(ValueError):
+        prime.generate_prime_factors(4.3)
--- prime.py
+++ prime.py
@@ -1,3 +1,7 @@
 """
 prime.py -- Write the application code here
 """
+
+def generate_prime_factors(value):
+    if not isinstance(value, int):
+        raise ValueError()
