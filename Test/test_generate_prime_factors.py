f78ced38f9dad97daf02cfaede2128714671c2ea
bezzy0515 <beselaere@hotmail.com>
2024-10-07 21:03:46 -0400

    Step #2 Complete

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -12,3 +12,17 @@ def test_data_not_integer():
         prime.generate_prime_factors("test")
     with pytest.raises(ValueError):
         prime.generate_prime_factors(4.3)
+
+
+"""
+Step #2
+Return empty list when 1 is passed.
+"""
+
+
+def test_empty_list_ret_with_1_passed():
+    null_test_list = []
+    assert null_test_list == prime.generate_prime_factors(1)
+
+
+
