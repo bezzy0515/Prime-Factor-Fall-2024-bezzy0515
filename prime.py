c09dc4066cae29ed83cdf756d51d65a121e48e7d
bezzy0515 <beselaere@hotmail.com>
2024-10-07 21:09:54 -0400

    Step #4 Complete

--- prime.py
+++ prime.py
@@ -12,3 +12,6 @@ def generate_prime_factors(value):
     if value == 2:
         prime_factors_list = [2]
         return prime_factors_list
+    if value == 3:
+        prime_factors_list = [3]
+        return prime_factors_list
