a3b0f40c872e280001a1f39aa492c3e365d24668
bezzy0515 <beselaere@hotmail.com>
2024-10-07 20:59:52 -0400

    Step #1 Complete

--- prime.py
+++ prime.py
@@ -6,24 +6,4 @@ prime.py -- Write the application code here
 def generate_prime_factors(value):
     if not isinstance(value, int):
         raise ValueError()
-    if value == 1:
-        prime_factors_list = []
-        return prime_factors_list
-    if value == 2:
-        prime_factors_list = [2]
-        return prime_factors_list
-    if value == 3:
-        prime_factors_list = [3]
-        return prime_factors_list
-    if value == 4:
-        prime_factors_list = [2, 2]
-        return prime_factors_list
-    if value == 6:
-        prime_factors_list = [2, 3]
-        return prime_factors_list
-    if value == 8:
-        prime_factors_list = [2, 2, 2]
-        return prime_factors_list
-    if value == 9:
-        prime_factors_list = [3, 3]
-        return prime_factors_list
+
